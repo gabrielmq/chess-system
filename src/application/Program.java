abf8749087dc27524ea46d089a5d5fa0fef7d10d
Gabriel <gabrielmqs04@gmail.com>
2018-08-02 17:40:02 -0300

    update method main

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch match = new ChessMatch();
-		UI.printBoard(match.getPieces());
+		
+		while (true) {
+			UI.printBoard(match.getPieces());
+			
+			System.out.print("\nSource: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.print("\nTarget: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = match.performChessMove(source, target);
+		}
 	}
 }
