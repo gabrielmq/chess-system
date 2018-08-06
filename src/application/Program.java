34bc843a99364128c97c52ee4bcd0473cd6b4630
gabriel <gabrielmqs04@gmail.com>
2018-08-06 13:00:29 -0300

    update main method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -14,11 +16,12 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch match = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 		
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(match);
+				UI.printMatch(match, captured);
 				
 				System.out.print("\nSource: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -31,6 +34,10 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 				
 				ChessPiece capturedPiece = match.performChessMove(source, target);
+				
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 			} 
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
