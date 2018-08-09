7447bb313661c59530c66132f9586b084877e520
Gabriel <gabrielmqs04@gmail.com>
2018-08-08 23:12:10 -0300

    update possibleMoves - especial move en passant

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Pawn extends ChessPiece {
 
-	public Pawn(Board board, Color color) {
+	private ChessMatch chessMatch;
+	
+	public Pawn(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
@@ -40,6 +44,18 @@ public class Pawn extends ChessPiece {
 			if (super.getBoard().positionExists(p) && super.isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
+			
+			// #especialmove en passant white
+			if (super.position.getRow() == 3) {
+				Position left = new Position(super.position.getRow(), super.position.getColumn() - 1);
+				if (super.getBoard().positionExists(left) && super.isThereOpponentPiece(left) && super.getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
+					mat[left.getRow() - 1][left.getColumn()] = true;
+				}
+				Position right = new Position(super.position.getRow(), super.position.getColumn() - 1);
+				if (super.getBoard().positionExists(right) && super.isThereOpponentPiece(right) && super.getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
+					mat[right.getRow() - 1][right.getColumn()] = true;
+				}
+			}
 		}
 		else {
 			p.setValues(super.position.getRow() + 1, super.position.getColumn());
@@ -64,6 +80,8 @@ public class Pawn extends ChessPiece {
 			if (super.getBoard().positionExists(p) && super.isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
+			
+			// #especialmove en passant black
 		}
 		
 		return mat;
